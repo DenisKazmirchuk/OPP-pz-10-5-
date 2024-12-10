3764074e0993ec1100efdb104c3f880dd81b884f
DenisKazmirchuk <161360416+DenisKazmirchuk@users.noreply.github.com>
2024-12-10 20:38:57 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 32, 22, 55, 48, 26, 54, 98 };
+
+        // Сума чисел
+        int sum = numbers.Sum();
+        Console.WriteLine($"Сума чисел: {sum}");
+
+        // Різниця чисел
+        int difference = numbers.Aggregate((a, b) => a - b);
+        Console.WriteLine($"Різниця чисел: {difference}");
+    }
+}
